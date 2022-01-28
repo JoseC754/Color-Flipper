382fbf4a354067a63bffc3d5622d5c9e72430d09
JoseC754 <90651035+JoseC754@users.noreply.github.com>
2022-01-28 14:34:45 -0700

    Add change of color

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-    const randomNumber = getRandomNumber ();
-    console.log(randomNumber);
+    const randomNumber = getRandomNumber();
+    //console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber () {
-    return Math.floor(Math.random() * color.lenght);
+    return Math.floor(Math.random() * colors.length);
 }
